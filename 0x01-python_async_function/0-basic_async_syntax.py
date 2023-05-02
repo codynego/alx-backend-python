703fed7010fb9ead0aefe1e1aeea5d3c94bb1dd6
codynego <emonenaabednego@gmail.com>
2023-05-02 18:23:20 +0000

    fixed an error with pep8

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,6 +5,16 @@ import random
 
 
 async def wait_random(max_delay: float = 10) -> float:
+    """
+    Waits for a random delay between 0 and
+    max_delay (inclusive) seconds and returns it.
+
+    Args:
+        max_delay: The maximum delay in seconds (inclusive).
+
+    Returns:
+        The delay value in seconds.
+    """
     delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
-    return (delay)
+    return delay
