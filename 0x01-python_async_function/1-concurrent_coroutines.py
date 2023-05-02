13cf2fa426955be32febd1f36e072e058bd6a138
codynego <emonenaabednego@gmail.com>
2023-05-02 18:49:28 +0000

    asyncio coroutines implementation

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import asyncio
+from typing import List
+from basic_async_syntax import wait_random
+
+
+async def wait_n(n: int, max_delay: float) -> List[float]:
+    """
+    Spawns `n` instances of `wait_random` with the specified
+    `max_delay` and returns a sorted list
+    of the resulting delays.
+
+    Args:
+        n: The number of times to spawn `wait_random`.
+        max_delay: The maximum delay value to use for each
+        `wait_random` instance.
+
+    Returns:
+        A sorted list of the resulting delays (float values).
+    """
+    # Create a list to hold the delay values
+    delays = []
+
+    # Create a list to hold the coroutines for wait_random
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+
+    # Use asyncio.gather() to run all the coroutines concurrently
+    results = await asyncio.gather(*coroutines)
+
+    # Sort the resulting delay values in ascending order
+    delays = sorted(results)
+
+    # Return the sorted list of delays
+    return delays
+
