5e02c46c54abd5b96da73b417565ac5884d4ec7e
codynego <emonenaabednego@gmail.com>
2023-05-02 20:59:09 +0500

    add module documentation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+
+"""
+execute multiple coroutines at the same time with async
+"""
+
 import asyncio
 from typing import List, Union
 wait_random = __import__('0-basic_async_syntax').wait_random
