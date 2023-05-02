19bf14d46b5c67542121e983607afcd9529b5275
codynego <emonenaabednego@gmail.com>
2023-05-02 20:25:03 +0500

    fixed a syntax errror

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,7 +2,7 @@
 
 import asyncio
 from typing import List
-from basic_async_syntax import wait_random
+from 0-basic_async_syntax import wait_random
 
 
 async def wait_n(n: int, max_delay: float) -> List[float]:
